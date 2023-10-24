ea90c8d948741bb61013929a24a0046fb93e6115
Kodekloner <billgateokoye@gmail.com>
2023-10-24 20:42:01 +0100

    Update

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *iter, *insert, *temp;
+
+	if (*list == NULL || (*list)->next == NULL || list == NULL)
+		return;
+
+	for (iter = (*list)->next; iter != NULL; iter = temp)
+	{
+		temp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
+		{
+			insert->next = iter->next;
+			if (iter->next != NULL)
+				iter->next->prev = insert;
+			iter->prev = insert->prev;
+			iter->next = insert;
+			if (insert->prev != NULL)
+				insert->prev->next = iter;
+			else
+				*list = iter;
+			insert->prev = iter;
+			insert = iter->prev;
+			print_list((const listint_t *)*list);
+		}
+	}
+}
