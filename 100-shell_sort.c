9f266984463f310ca42a60f49039145c03e2c1ed
Kodekloner <billgateokoye@gmail.com>
2023-10-25 03:07:09 +0100

    Updated

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,20 +1,6 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
  * shell_sort - Sort an array of integers in ascending
  *              order using the shell sort algorithm.
  * @array: An array of integers.
@@ -24,23 +10,25 @@ void swap_ints(int *a, int *b)
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	size_t gp, x, y;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (gap = 1; gap < (size / 3);)
-		gap = gap * 3 + 1;
+	for (gp = 1; gp < (size / 3);)
+		gp = gp * 3 + 1;
 
-	for (; gap >= 1; gap /= 3)
+	for (; gp >= 1; gp /= 3)
 	{
-		for (i = gap; i < size; i++)
+		for (x = gp; x < size; x++)
 		{
-			j = i;
-			while (j >= gap && array[j - gap] > array[j])
+			y = x;
+			while (y >= gp && array[y - gp] > array[y])
 			{
-				swap_ints(array + j, array + (j - gap));
-				j -= gap;
+				tmp = array[y];
+        array[y] = array[y - gp];
+        array[y - gp] = tmp;
+				y -= gp;
 			}
 		}
 		print_array(array, size);
