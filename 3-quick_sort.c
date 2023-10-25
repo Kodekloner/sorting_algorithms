029f2bf7228879c6749fd24bf656dfbe2c9e5fb1
Kodekloner <billgateokoye@gmail.com>
2023-10-25 02:57:07 +0100

    Updated

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -24,7 +24,7 @@ void swapping_ints(int *x, int *y)
  *
  * Return: The final partition index.
  */
-int partition(int *array, size_t size, int x, int y)
+int partition(int *array, size_t sz, int x, int y)
 {
 	int *mid, up, down;
 
@@ -36,7 +36,7 @@ int partition(int *array, size_t size, int x, int y)
 			if (up < down)
 			{
 				swapping_ints(array + down, array + up);
-				print_array(array, size);
+				print_array(array, sz);
 			}
 			up++;
 		}
@@ -45,7 +45,7 @@ int partition(int *array, size_t size, int x, int y)
 	if (array[up] > *mid)
 	{
 		swapping_ints(array + up, mid);
-		print_array(array, size);
+		print_array(array, sz);
 	}
 
 	return (up);
@@ -54,21 +54,21 @@ int partition(int *array, size_t size, int x, int y)
 /**
  * sort - Implement the quicksort algorithm through recursion.
  * @array: An array of integers to sort.
- * @size: The size of the array.
+ * @sz: The size of the array.
  * @x: The starting index of the array partition to order.
  * @y: The ending index of the array partition to order.
  *
  * Description: Uses the Lomuto partition scheme.
  */
-void sort(int *array, size_t size, int x, int y)
+void sort(int *array, size_t sz, int x, int y)
 {
 	int part;
 
 	if (y - x > 0)
 	{
-		part = partition(array, size, x, y);
-		sort(array, size, x, part - 1);
-		sort(array, size, part + 1, y);
+		part = partition(array, sz, x, y);
+		sort(array, sz, x, part - 1);
+		sort(array, sz, part + 1, y);
 	}
 }
 
